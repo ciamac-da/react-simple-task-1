5f2a698db3e86157d61149edb4ef963da6a47040
ciamac-da <ciamac.davoudi@gmail.com>
2021-04-02 18:37:30 +0200

    Verified by Marti4n v1.0.11

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,45 @@
-import React, {Component} from "react";
+import React, { useState } from "react";
 import Validation from "../components/Validation/Validation";
-import Char from '../components/Char/Char';
-import './App.css';
+import Char from "../components/Char/Char";
+import "./App.css";
 
-class App extends Component {
-  constructor(props){
-    super(props)
-      this.state={userInput:""}
-  }
+const App = () => {
+  const [state, setState] = useState("");
 
+  const inputChangedHandler = (event) => {
+    return setState(event.target.value);
+  };
 
-  inputChangedHandler = (event) => {
-    this.setState({userInput: event.target.value})
-  }
+  const deleteInputCharHandler = (index) => {
+    const text = state.split("");
+    text.splice(index, 1);
+    const updatedText = text.join("");
+    return setState(updatedText);
+  };
 
-  deleteInputCharHandler = (index) =>{
-  const text = this.state.userInput.split("")
-  text.splice(index, 1)
-  const updatedText = text.join("");
-  this.setState({userInput:updatedText})
-  }
-  
-  render(){
-    const charList = this.state.userInput.split("").map((char, index)=>{
-      return <Char 
-      character={char}
-      clicked={()=> this.deleteInputCharHandler(index)}
-      key={index} />
-    })
-
+  const charList = state.split("").map((char, index) => {
     return (
-      <div className="App">
-      <input
-        onChange={this.inputChangedHandler}
-        value={this.state.userInput}
-        type="text"
-        style={{textAlign:"center"}}
-
+      <Char
+        character={char}
+        clicked={() => deleteInputCharHandler(index)}
+        key={index}
       />
-      <p style={{fontSize:"24px", fontWeight:"bolder"}}>{this.state.userInput}</p>
-      <Validation inputLength={this.state.userInput.length} />
-     {charList}
+    );
+  });
+
+  return (
+    <div className="App">
+      <input
+        onChange={inputChangedHandler}
+        value={state}
+        type="text"
+        style={{ textAlign: "center" }}
+      />
+      <p style={{ fontSize: "24px", fontWeight: "bolder" }}>{state}</p>
+      <Validation inputLength={state.length} />
+      {charList}
     </div>
   );
-  }
-}
+};
 
 export default App;
